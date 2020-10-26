937a81082ce3fc01528a6f8d720a67e242366026
Emilliana Paredes <emilliana.paredes@gmail.com>
2020-10-25 21:24:34 -0500

    commit tree 8902c77282cb262a38fff78d45c31958e6ca9bf8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+def badge_maker(name)
+    name = "Arel"
+        puts "Hello, my name is #{name}."
+        return "Hello, my name is Arel."
+end
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each{|attendee| badges << "Hello, my name is #{attendee}."}
+    badges
+    end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index{|name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+    room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end 
+end
+
+    
+   
+
+    
+    
+
+
+
+
+
+
